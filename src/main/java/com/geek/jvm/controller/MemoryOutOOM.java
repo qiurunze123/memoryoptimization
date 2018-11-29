2343c5c0b82d2678d8bcb39150dcce34048b45e4
qiurunze <qiurunze@youxin.com>
2018-11-29 17:32:54 +0800

    提交oom内存泄漏

---
+++ src/main/java/com/geek/jvm/controller/MemoryOutOOM.java
@@ -0,0 +1,24 @@
+package com.geek.jvm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/memory")
+public class MemoryOutOOM {
+
+
+    @RequestMapping(value = "/oom")
+    public String user() {
+        List<User> users = new ArrayList<>();
+        int i =0;
+        while(true){
+            users.add(new User(UUID.randomUUID().toString(),i++));
+        }
+    }
+
+}
