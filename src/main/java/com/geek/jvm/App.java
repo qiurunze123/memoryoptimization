bfb00e20193fa457889ec761c342366eb374a458
qiurunze <qiurunze@youxin.com>
2020-01-09 15:53:52 +0800

    jvm 了解

--- src/main/java/com/geek/jvm/App.java
+++ src/main/java/com/geek/jvm/App.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Hello world!
